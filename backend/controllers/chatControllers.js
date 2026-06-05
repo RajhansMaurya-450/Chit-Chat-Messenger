eb378581ccb108aef7201cbbe6b99a3cfdc641c4
Raj <rajmaurya3091@gmail.com>
2026-06-05 23:37:40 +0530

    searching bug fix

--- backend/controllers/chatControllers.js
+++ backend/controllers/chatControllers.js
@@ -6,10 +6,10 @@ const accessChat = asyncHandler(async (req, res) => {
     const { userId } = req.body;
 
     if (!userId) {
-        console.log("UserId param not snet with request");
+        console.log("UserId param not sent with request");
         return res.sendStatus(400);
     }
-
+    //logic for finding chat for a specific user........
     var isChat = await Chat.find({
         isGroupChat: false,
         $and: [
